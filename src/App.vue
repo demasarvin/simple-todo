5ed2d1171dae31e39787e41ecb473f8f8d786205
Demas Arvin <demasarvin11@gmail.com>
2022-06-18 10:36:58 +0700

    Fix flex-wrap

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,28 @@
 <template>
   <div
-    class="container mx-auto text-bold text-4xl my-4 border rounded-md p-6 border-black"
+    class="container mx-auto"
   >
-    <h2 class="text-center">SIMPLE TODO APP</h2>
-    <div class="flex justify-center items-center pt-4">
-      <input
-        v-model="todo"
-        type="text"
-        class="p-4 border border-black rounded-md text-xl w-80"
-        @keyup.enter="add"
-      />
-      <button
-        class="py-2 px-4 bg-green-600 rounded-md text-white ml-6 hover:bg-green-800"
-        @click="add"
-      >
-        Add
-      </button>
+    <div class="text-bold text-4xl mx-4 my-4 border rounded-md p-2 lg:p-6 border-black">
+      <h2 class="text-center">SIMPLE TO DO APP</h2>
+      <div class="lg:flex flex-wrap text-center justify-center items-center pt-4">
+        <input
+          v-model="todo"
+          type="text"
+          class="p-3 border border-black rounded-md text-xl w-auto"
+          @keyup.enter="add"
+        />
+        <div class="">
+          <button
+          class="mt-4 lg:mt-0 py-2 px-4 bg-green-600 rounded-md text-white lg:ml-6 hover:bg-green-800"
+          @click="add"
+        >
+          Add
+        </button>
+        </div>
+      </div>
+      <list :todos="list" @doneTodo="doneTodo" @deleteTodo="deleteTodo" />
+      <small>Total TO DO : {{ totalTodo }}</small>
     </div>
-    <list :todos="list" @doneTodo="doneTodo" @deleteTodo="deleteTodo" />
-    <small>Total TODO : {{ totalTodo }}</small>
   </div>
 </template>
 
--- src/components/List.vue
+++ src/components/List.vue
@@ -6,21 +6,21 @@
       class="mx-auto p-4 mb-2 border rounded-md border-black"
     >
       <div class="flex justify-between items-center text-2xl">
-        <div>
+        <div class="flex-wrap w-4/5">
           <div v-if="todo.isDone">
             <del>{{ todo.activity }}</del>
           </div>
           <div v-else>{{ todo.activity }}</div>
         </div>
-        <div>
+        <div class="text-center w-1/5 lg:space-x-4">
           <button
-            class="py-2 px-4 bg-blue-600 rounded-md text-white ml-6 hover:bg-blue-800"
+            class="py-2 px-4 bg-blue-600 rounded-md text-white hover:bg-blue-800"
             @click="done(index)"
           >
             ✓
           </button>
           <button
-            class="py-2 px-4 bg-red-600 rounded-md text-white ml-6 hover:bg-red-800"
+            class="mt-2 lg:mt-0 py-2 px-4 bg-red-600 rounded-md text-white hover:bg-red-800"
             @click="deleteTodo(index)"
           >
             X
